c180cf64459b0f9b1b45d6572b69f915a7542574
cybera1chemist <1849770138@qq.com>
2026-02-19 16:58:17 +0800

    修复了诸葛无法正常生成的bug

--- Assets/Scripts/Enemy/SpecialEnemy/Zhuge.cs
+++ Assets/Scripts/Enemy/SpecialEnemy/Zhuge.cs
@@ -11,7 +11,6 @@ public class Zhuge : MonoBehaviour
     private SpriteRenderer sr;
     private Dialog dialog;
     private GameStatsManager manager;
-    public EnemyData enemyData;
 
     void Start()
     {
